e256530e5cff0c88947e06a6763dbb814b9877d3
张惠棣 <1412779356@qq.com>
2020-06-20 21:54:09 +0800

    发布评论

---
+++ app/resources/article/comment.py
@@ -0,0 +1,33 @@
+from flask import g
+from flask_restful import Resource
+from flask_restful.inputs import regex
+from flask_restful.reqparse import RequestParser
+
+from app import db
+from models.article import Comment, Article
+from utils.decorators import login_required
+
+
+class CommentsResource(Resource):
+    method_decorators = {'post':[login_required]}
+
+    def post(self):
+        """发布品论"""
+
+        userid = g.userid
+        parser=RequestParser()
+        parser.add_argument('target', required=True,location='json',type= int)
+        parser.add_argument('content', required=True,location='json',type= regex(r'.+'))
+        args=parser.parse_args()
+        target = args.target
+        content =args.content
+
+        #生成评论记录
+        comment=Comment(user_id=userid,article_id=target,content=content,parent_id=0)
+        db.session.commit()
+
+        Article.query.filter(Article.id ==target).update({'comment_count':Article.comment_count+1})
+        db.session.commit()
+
+        #
+        return {'com_id':comment.id,'target':target}
