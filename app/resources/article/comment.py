e6f40eb9dd739d974fe95a804d9e28c6117f957a
张惠棣 <1412779356@qq.com>
2020-06-21 15:02:01 +0800

    回复评论v1.0

--- app/resources/article/comment.py
+++ app/resources/article/comment.py
@@ -21,19 +21,36 @@ class CommentsResource(Resource):
         parser.add_argument('target', required=True, location='json', type=int)
         # 正则表达式：.代表任何内容，+ 至少一位
         parser.add_argument('content', required=True, location='json', type=regex(r'.+'))
+        parser.add_argument('parent_id', location='json', type=int)
         args = parser.parse_args()
-        target = args.target
-        content = args.content
+        target = args.target  # 文章id
+        content = args.content  # 评论内容
+        parent_id = args.parent_id  # 父评论id （如果发布子评论，则需要设置）
 
-        # 生成评论记录
-        comment = Comment(user_id=userid, article_id=target, content=content, parent_id=0)
-        db.session.add(comment)
-        # 让文章评论数量加1#
-        Article.query.filter(Article.id == target).update({'comment_count': Article.comment_count + 1})
-        db.session.commit()
+        # 判断发布评论\子评论
+        if parent_id:  # 发布子评论
+            # 新增子评论数据
+            sub_comment = Comment(user_id=userid, article_id=target, content=content, parent_id=parent_id)
 
-        #
-        return {'com_id': comment.id, 'target': target}
+            db.session.add(sub_comment)
+
+            # 让父评论回复数量+1
+            Comment.query.filter(Comment.id == parent_id).update({
+                'reply_count': Comment.reply_count + 1
+            })
+            db.session.commit()
+
+            return {'com_id': sub_comment.id, 'target': target, 'parent_id': parent_id}
+        else:
+            # 新增评论数据
+            comment = Comment(user_id=userid, article_id=target, content=content, parent_id=0)
+            db.session.add(comment)
+            # 让文章评论数量加1#
+            Article.query.filter(Article.id == target).update({'comment_count': Article.comment_count + 1})
+            db.session.commit()
+
+            #
+            return {'com_id': comment.id, 'target': target}
 
     def get(self):
         """获取评论列表"""
@@ -61,21 +78,21 @@ class CommentsResource(Resource):
             .filter(Comment.article_id == source, Comment.id > offset) \
             .limit(limit).all()
         # .order_by(Comment.like_count.desc())\
-        common_list=[]
+        common_list = []
         for item in data:
-            common_dict={
-              'com_di': item.id,
-              'aut_id': item.user_id,
-              'aut_name': item.name,
-              'aut_photo': item.profile_photo,
-              'pubdate': item.ctime.isoformat(),
-              'content': item.content,
-              'reply_count': item.reply_count,
-              'like_count': item.like_count
+            common_dict = {
+                'com_di': item.id,
+                'aut_id': item.user_id,
+                'aut_name': item.name,
+                'aut_photo': item.profile_photo,
+                'pubdate': item.ctime.isoformat(),
+                'content': item.content,
+                'reply_count': item.reply_count,
+                'like_count': item.like_count
             }
             common_list.append(common_dict)
-        #使其按照评论列表点赞数从大到小排列
-        common_list.sort(key=lambda obj:obj['like_count'], reverse=True )
+        # 使其按照评论列表点赞数从大到小排列
+        common_list.sort(key=lambda obj: obj['like_count'], reverse=True)
 
         # common_list = [{
         #     'com_di': item.id,
