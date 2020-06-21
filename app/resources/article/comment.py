f63632e9514434993d5e8d57b4c429689ef9f238
张惠棣 <1412779356@qq.com>
2020-06-21 11:53:16 +0800

    增加评论列表错误修改

--- app/resources/article/comment.py
+++ app/resources/article/comment.py
@@ -11,74 +11,73 @@ from utils.decorators import login_required
 
 
 class CommentsResource(Resource):
-    method_decorators = {'post':[login_required]}
+    method_decorators = {'post': [login_required]}
 
     def post(self):
         """发布品论"""
-        #获取参数
+        # 获取参数
         userid = g.userid
-        parser=RequestParser()
-        parser.add_argument('target', required=True,location='json',type= int)
+        parser = RequestParser()
+        parser.add_argument('target', required=True, location='json', type=int)
         # 正则表达式：.代表任何内容，+ 至少一位
-        parser.add_argument('content', required=True,location='json',type= regex(r'.+'))
-        args=parser.parse_args()
+        parser.add_argument('content', required=True, location='json', type=regex(r'.+'))
+        args = parser.parse_args()
         target = args.target
-        content =args.content
+        content = args.content
 
-        #生成评论记录
-        comment=Comment(user_id=userid,article_id=target,content=content,parent_id=0)
+        # 生成评论记录
+        comment = Comment(user_id=userid, article_id=target, content=content, parent_id=0)
         db.session.add(comment)
         # 让文章评论数量加1#
-        Article.query.filter(Article.id ==target).update({'comment_count':Article.comment_count+1})
+        Article.query.filter(Article.id == target).update({'comment_count': Article.comment_count + 1})
         db.session.commit()
 
         #
-        return {'com_id':comment.id,'target':target}
+        return {'com_id': comment.id, 'target': target}
 
     def get(self):
         """获取评论列表"""
         # 获取参数
         parser = RequestParser()
-        parser.add_argument('source', required=True, location='json', type=int)
-        parser.add_argument('offset',default =0 , location='json', type=int)
-        #基于该字段进行分页查询
-        parser.add_argument('limit', required=True, location='json', type=int)
+        parser.add_argument('source', required=True, location='args', type=int)
+        parser.add_argument('offset', default=0, location='args', type=int)
+        # 基于该字段进行分页查询
+        parser.add_argument('limit', default=10, location='args', type=int)
         args = parser.parse_args()
         source = args.source
         offset = args.offset
         limit = args.limit
 
-
         # 数据库查询 该文章的评论 & 分页　＆（评论ｉｄ＞　offset参数）
         data = db.session.query(Comment.id,
-                         Comment.user_id,
-                         User.name,
-                         User.profile_photo,
-                         Comment.ctime,
-                         Comment.content,
-                         Comment.reply_count,
-                         Comment.like_count)\
-        .join(User,Comment.user_id ==User.id)\
-        .filter({Comment.article_id == source, Comment.id > offset})\
-        .limit(limit).all()
+                                Comment.user_id,
+                                User.name,
+                                User.profile_photo,
+                                Comment.ctime,
+                                Comment.content,
+                                Comment.reply_count,
+                                Comment.like_count) \
+            .join(User, Comment.user_id == User.id) \
+            .filter(Comment.article_id == source, Comment.id > offset) \
+            .limit(limit).all()
 
         common_list = [{
-            'com_di':item.id,
-            'aut_id':item.user_id,
-            'aut_name':item.name,
-            'aut_photo':item.profile_photo,
+            'com_di': item.id,
+            'aut_id': item.user_id,
+            'aut_name': item.name,
+            'aut_photo': item.profile_photo,
             'pubdate': item.ctime.isoformat(),
-            'content':item.content,
-            'reply_count':item.reply_count,
-            'like_count':item.like_count
+            'content': item.content,
+            'reply_count': item.reply_count,
+            'like_count': item.like_count
         } for item in data]
-        #构造响应数据
-        count = Comment.query.filter(Comment.article_id ==source).count()
+        # 构造响应数据
+        count = Comment.query.filter(Comment.article_id == source).count()
 
-        end_comment = Comment.query.options(load_only(Comment.id))\
-            .filter(Comment.article_id == source)\
+        end_comment = Comment.query.options(load_only(Comment.id)) \
+            .filter(Comment.article_id == source) \
             .order_by(Comment.id.desc()).first()
         end_id = end_comment.id if end_comment else None
         last_id = data[-1].id if data else None
-        return  {'results':common_list , 'total_count':count,
-                 'end_id':end_id, 'last_id':last_id}
+        return {'results': common_list, 'total_count': count,
+                'end_id': end_id, 'last_id': last_id}
