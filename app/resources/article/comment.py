0a8b81f216da19ce216868509a58dcab3c06af35
张惠棣 <1412779356@qq.com>
2020-06-21 20:37:10 +0800

    回复列表v2.0

--- app/resources/article/comment.py
+++ app/resources/article/comment.py
@@ -16,6 +16,7 @@ class CommentsResource(Resource):
     def post(self):
         """发布评论"""
         # 获取参数
+
         userid = g.userid
         parser = RequestParser()
         parser.add_argument('target', required=True, location='json', type=int)
@@ -26,14 +27,17 @@ class CommentsResource(Resource):
         target = args.target  # 文章id
         content = args.content  # 评论内容
         parent_id = args.parent_id  # 父评论id （如果发布子评论，则需要设置）
-
+        print(target,parent_id,content)
         # 判断发布评论\子评论
         if parent_id:  # 发布子评论
             # 新增子评论数据
-            sub_comment = Comment(user_id=userid, article_id=target, content=content, parent_id=parent_id)
+            sub_comment = Comment(user_id=userid,
+                                  article_id=target,
+                                  content=content,
+                                  parent_id=parent_id)
 
             db.session.add(sub_comment)
-
+            print(sub_comment)
             # 让父评论回复数量+1
             Comment.query.filter(Comment.id == parent_id).update({
                 'reply_count': Comment.reply_count + 1
@@ -43,7 +47,8 @@ class CommentsResource(Resource):
             return {'com_id': sub_comment.id, 'target': target, 'parent_id': parent_id}
         else:
             # 新增评论数据
-            comment = Comment(user_id=userid, article_id=target, content=content, parent_id=0)
+            comment = Comment(user_id=userid, article_id=target,
+                              content=content, parent_id=0)
             db.session.add(comment)
             # 让文章评论数量加1#
             Article.query.filter(Article.id == target).update({'comment_count': Article.comment_count + 1})
@@ -57,6 +62,7 @@ class CommentsResource(Resource):
         # 获取参数
         parser = RequestParser()
         parser.add_argument('source', required=True, location='args', type=int)  # a 文章id 、c 夫评论id
+        #评论id
         parser.add_argument('offset', default=0, location='args', type=int)
         # 基于该字段进行分页查询
         parser.add_argument('limit', default=10, location='args', type=int)
@@ -84,9 +90,7 @@ class CommentsResource(Resource):
             count = Comment.query.filter(Comment.article_id == source, Comment.parent_id == 0).count()
             # 所有列表中最后一条回复的记录
             # end_comment = Comment.query.options(load_only(Comment.id)) .filter(Comment.article_id == source) .order_by(Comment.id.desc()).first()
-            end_comment = Comment.query.options(Comment.id) \
-                .filter(Comment.article_id == source, Comment.parent_id == 0) \
-                .order_by(Comment.id.desc()).first()
+            end_comment =Comment.query.options(load_only(Comment.id)).filter(Comment.article_id == source, Comment.parent_id == 0) .order_by(Comment.id.desc()).first()
         else:  # 回复列表
             # 某条评论的的回复列表
             data = db.session.query(Comment.id,
@@ -104,7 +108,7 @@ class CommentsResource(Resource):
             count = Comment.query.filter(Comment.parent_id == source).count()
             # 某条评论的的回复列表中最后一条回复的记录
             end_comment = Comment.query.options(load_only(Comment.id)).filter(Comment.parent_id == source) \
-                .order_by(Comment.iddesc()).first()
+                .order_by(Comment.id.desc()).first()
             """
             common_list = []
             for item in data:
@@ -139,6 +143,7 @@ class CommentsResource(Resource):
         common_list.sort(key=lambda obj: obj['like_count'], reverse=True)
 
         end_id = end_comment.id if end_comment else None
+        # 本次请求中的最后一条id
         last_id = data[-1].id if data else None
         return {'results': common_list, 'total_count': count,
                 'end_id': end_id, 'last_id': last_id}
