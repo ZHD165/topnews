912b1735ea2b3db9a2e8214fa61e55ee7a0cffca
张惠棣 <1412779356@qq.com>
2020-06-21 15:47:14 +0800

    回复列表v1.0

--- app/resources/article/comment.py
+++ app/resources/article/comment.py
@@ -14,7 +14,7 @@ class CommentsResource(Resource):
     method_decorators = {'post': [login_required]}
 
     def post(self):
-        """发布品论"""
+        """发布评论"""
         # 获取参数
         userid = g.userid
         parser = RequestParser()
@@ -56,28 +56,72 @@ class CommentsResource(Resource):
         """获取评论列表"""
         # 获取参数
         parser = RequestParser()
-        parser.add_argument('source', required=True, location='args', type=int)
+        parser.add_argument('source', required=True, location='args', type=int)  # a 文章id 、c 夫评论id
         parser.add_argument('offset', default=0, location='args', type=int)
         # 基于该字段进行分页查询
         parser.add_argument('limit', default=10, location='args', type=int)
+        parser.add_argument('type', required=True, location='args', type=regex(r'[ac]'))
         args = parser.parse_args()
         source = args.source
         offset = args.offset
         limit = args.limit
+        type = args.type
+        # 判断获取 评论列表 回复列表
+        if type == 'a':
+            data = db.session.query(Comment.id,
+                                    Comment.user_id,
+                                    User.name,
+                                    User.profile_photo,
+                                    Comment.ctime,
+                                    Comment.content,
+                                    Comment.reply_count,
+                                    Comment.like_count) \
+                .join(User, Comment.user_id == User.id) \
+                .filter(Comment.article_id == source, Comment.id > offset, Comment.parent_id == 0) \
+                .limit(limit).all()
 
-        # 数据库查询 该文章的评论 & 分页　＆（评论ｉｄ＞　offset参数）
-        data = db.session.query(Comment.id,
-                                Comment.user_id,
-                                User.name,
-                                User.profile_photo,
-                                Comment.ctime,
-                                Comment.content,
-                                Comment.reply_count,
-                                Comment.like_count) \
-            .join(User, Comment.user_id == User.id) \
-            .filter(Comment.article_id == source, Comment.id > offset) \
-            .limit(limit).all()
-        # .order_by(Comment.like_count.desc())\
+            # 查看评论总数
+            count = Comment.query.filter(Comment.article_id == source, Comment.parent_id == 0).count()
+            # 所有列表中最后一条回复的记录
+            # end_comment = Comment.query.options(load_only(Comment.id)) .filter(Comment.article_id == source) .order_by(Comment.id.desc()).first()
+            end_comment = Comment.query.options(Comment.id) \
+                .filter(Comment.article_id == source, Comment.parent_id == 0) \
+                .order_by(Comment.id.desc()).first()
+        else:  # 回复列表
+            # 某条评论的的回复列表
+            data = db.session.query(Comment.id,
+                                    Comment.user_id,
+                                    User.name,
+                                    User.profile_photo,
+                                    Comment.ctime,
+                                    Comment.content,
+                                    Comment.reply_count,
+                                    Comment.like_count) \
+                .join(User, Comment.user_id == User.id) \
+                .filter(Comment.parent_id == source, Comment.id > offset) \
+                .limit(limit).all()
+            # 某条评论的的回复总数
+            count = Comment.query.filter(Comment.parent_id == source).count()
+            # 某条评论的的回复列表中最后一条回复的记录
+            end_comment = Comment.query.options(load_only(Comment.id)).filter(Comment.parent_id == source) \
+                .order_by(Comment.iddesc()).first()
+            """
+            common_list = []
+            for item in data:
+                common_dict = {
+                    'com_di': item.id,
+                    'aut_id': item.user_id,
+                    'aut_name': item.name,
+                    'aut_photo': item.profile_photo,
+                    'pubdate': item.ctime.isoformat(),
+                    'content': item.content,
+                    'reply_count': item.reply_count,
+                    'like_count': item.like_count
+                }
+                common_list.append(common_dict)
+            # 使其按照评论列表点赞数从大到小排列
+            common_list.sort(key=lambda obj: obj['like_count'], reverse=True)
+            """
         common_list = []
         for item in data:
             common_dict = {
@@ -94,21 +138,6 @@ class CommentsResource(Resource):
         # 使其按照评论列表点赞数从大到小排列
         common_list.sort(key=lambda obj: obj['like_count'], reverse=True)
 
-        # common_list = [{
-        #     'com_di': item.id,
-        #     'aut_id': item.user_id,
-        #     'aut_name': item.name,
-        #     'aut_photo': item.profile_photo,
-        #     'pubdate': item.ctime.isoformat(),
-        #     'content': item.content,
-        #     'reply_count': item.reply_count,
-        #     'like_count': item.like_count
-        # } for item in data ]
-        # 构造响应数据
-        count = Comment.query.filter(Comment.article_id == source).count()
-
-        # end_comment = Comment.query.options(load_only(Comment.id)) .filter(Comment.article_id == source) .order_by(Comment.id.desc()).first()
-        end_comment = Comment.query.options(load_only(Comment.article_id == source)).order_by(Comment.id.desc()).first()
         end_id = end_comment.id if end_comment else None
         last_id = data[-1].id if data else None
         return {'results': common_list, 'total_count': count,
